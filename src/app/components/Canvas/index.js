f4441365108087a01bee3b911e6a89e4ebee4a57
unknown <m.cottrelle@mediacall.fr>
2025-04-25 17:55:04 +0200

    add: canvas base logic

--- src/app/components/Canvas/index.js
+++ src/app/components/Canvas/index.js
@@ -1,1 +1,32 @@
 //Class qui permet de génerer tous les canvas du site (preloader ????)
+import * as THREE from "three";
+export default class Canvas {
+  constructor() {
+    this.createRenderer();
+  }
+
+  createRenderer() {
+    this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  createCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.z = 5;
+  }
+  createScene() {
+    this.scene = new THREE.Scene();
+  }
+
+  onResize() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
+}
