ff61a4ab42303c6b504c1b5b1d468414a591d860
unknown <m.cottrelle@mediacall.fr>
2025-09-08 09:17:03 +0200

    fix: deezers fetch -> allorigins proxy

--- src/app/pages/Album/index.js
+++ src/app/pages/Album/index.js
@@ -80,7 +80,9 @@ export class Album extends Page {
     const url = `https://api.deezer.com/search?q=${query}`;
 
     try {
-      const res = await fetch(url);
+      const res = await fetch(
+        `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
+      );
       const data = await res.json();
 
       if (data?.data?.length > 0 && data.data[0].preview) {
